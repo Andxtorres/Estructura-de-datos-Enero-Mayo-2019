2703fd085bbd508c0aab40ebeb67bdf3a146f34e
Andrés Torres <andres.torres@nauroo.com>
2019-02-08 00:03:51 -0600

    Linked List

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+	
+	public static void main(String args[]) {
+		LinkedList<Alumno> linkedList= new LinkedList<>();
+		Alumno a= new Alumno("A0121194","Andres");
+		linkedList.addElementAtStart(new Alumno("A0121193","Andres"));
+		linkedList.addElementAtStart(new Alumno("A0121192","Andres"));
+		linkedList.addElementAtStart(new Alumno("A0121191","Andres"));
+		linkedList.addElementAtEnd(a);
+		//linkedList.removeFirst();
+		linkedList.removeLast();
+		System.out.println(a.toString());
+		System.out.println("");
+		linkedList.print();
+		System.out.println("Element count: " +linkedList.count());
+		System.out.println("Final element: "+linkedList.getLastNode());
+		System.out.println("First element: "+linkedList.getFirstNode());
+		System.out.println("Element at: "+linkedList.getElementAtIndex(5));
+	}
+}
